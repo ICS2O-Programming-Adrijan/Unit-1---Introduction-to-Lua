0788967c1eea21a226cf51f1e6924aafb518d801
Adrijan Vranjkovic <adrijan.vranjkovic@ocsbstudent.ca>
2020-02-26 14:03:38 -0500

    Initial commit

---
+++ HelloWorldAdrijan/main.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+-- print "Hello World" to the command terminal
+print("****Hello Adrijan!")
+
+--Hide the status bar
+display.setstatus(Display.HiddenStatusBar)
+
+-- Set the colour of the backround
+Display.setDefault ("background", 43/255, 249/255, 199/255)
