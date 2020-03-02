fb6d1f0122720865cded76bb736ba4746ff17993
Adrijan Vranjkovic <adrijan.vranjkovic@ocsbstudent.ca>
2020-03-02 15:25:32 -0500

    adde dMoving Images

--- MovingImagesAdrijan/main.lua
+++ MovingImagesAdrijan/main.lua
@@ -42,3 +42,27 @@ end
 Runtime:addEventListener("enterFrame", MoveShip)
 
 --character image with width and height
+local star = display.newImageRect("Images/star.png", 200, 200)
+
+--set the image to be visible
+star.alpha = 1
+
+--set the initial x and y position of the star
+star.x = 1024
+star.y = display.contentHeight/8
+
+newScrollSpeed = -3
+--Change the scroll speed
+--Function: MoveShip
+--Input: This function accepts an event listener
+--Output: none
+--Description: this function adds the scroll speed to the x-value of the ship
+local function moveStar(event)
+	--add the scroll speed to the x-valueof the ship
+	star.x = star.x + newScrollSpeed
+	--change the transparentcy of the ship every time it moves
+	star.alpha = star.alpha - 0.01
+end
+
+--movestar will be called over and over again
+Runtime:addEventListener("enterFrame", moveStar) 
