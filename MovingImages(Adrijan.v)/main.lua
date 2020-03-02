b75b4506abc0b8a1cabf8ff5e9da97813fd9f812
Adrijan Vranjkovic <adrijan.vranjkovic@ocsbstudent.ca>
2020-03-02 14:32:46 -0500

    addedMovingImages

---
+++ MovingImages(Adrijan.v)/main.lua
@@ -0,0 +1,42 @@
+-- Title: MovingObjects
+-- Name: Adrijan.V
+-- Course: ICS2O/3C
+-- This program moves 
+--a beetleship across the screen and
+-- then makes it fade out.
+-- I added a second object that
+-- moves in a different direction
+--fades out and grows in size.
+
+-- hide the status background
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables
+scrollSpeed = 3
+
+-- background image with width and height
+local backgroundImage = display.newImageRect("Images/background.png",2048, 1536)
+
+--character image with width and height
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+
+-- set the image to be transparent
+beetleship.alpha = 0
+
+--set the initial x and y position of beetleship
+beetleship.x = 0
+beetleship.y = display.contentHeight/3
+
+-- Function: MoveShip
+--Input: this function accepts an event listener
+--Output: none 
+--Description This function adds the scroll speed to the x-value of the ship
+local function MoveShip(event)
+	-- add the scroll speed to the x-value of the ship
+	beetleship.x = beetleship.x + scrollSpeed
+	-- change the transparentcy of the ship every time it moves so that it fades out
+	beetleship.alpha = beetleship.alpha + 0.01
+end
+
+--MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame ", MoveShip)
