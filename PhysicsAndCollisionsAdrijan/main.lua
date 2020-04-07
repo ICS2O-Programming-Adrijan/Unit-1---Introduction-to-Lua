abe86761e375e5aa254914b1fd1aff92ecf44339
Adrijan Vranjkovic <61517053+s275852@users.noreply.github.com>
2020-04-07 10:07:51 -0400

    added physics and collisions

--- PhysicsAndCollisionsAdrijan/main.lua
+++ PhysicsAndCollisionsAdrijan/main.lua
@@ -33,7 +33,7 @@ physics.addBody(ground, "static", {friction=0.5, bounce=0.3})
 local beam = display.newImage("Images/beam_long.png", display.contentWidth/2, display.contentHeight/10)
 
 --set the x and y position
-beam.x = display.contentCenterX/5
+beam.x = 80
 beam.y = display.contentCenterY*1.65
 
 --rotate the beam -60 degrees so its on an angle
@@ -45,6 +45,25 @@ beam:toBack()
 --add to physics
 physics.addBody(beam, "static", {friction=0.5, bounce=0.3})
 ------------------------------------------------------------
+local beam2 = display.newImage("Images/beam_long.png", 900, 680)
+
+--rotate the beam 
+beam2:rotate(-25)
+
+
+--set the x and y position
+beam.x = 80
+beam.y = display.contentCenterY*1.65
+
+--send to back 
+beam2:toBack()
+
+--add to physics
+physics.addBody(beam2, "static", {friction=1, bounce=1})
+
+
+
+----------------------------------------------------------------
 --create bkg
 local bkg = display.newImage("Images/bkg.png", 0, 0)
 
@@ -58,6 +77,7 @@ bkg.height = 768
 
 --send to back
 bkg:toBack()
+------------------------------------------------------------
 
 ---------------------------------------------------------------
 --FUNCTIONS
@@ -65,22 +85,23 @@ bkg:toBack()
 -- create the first ball
 local function FirstBall()
 	-- creating FirstBall
-	local ball1 = display.newImage("physics/super_ball.png", 0, 0)
+	local ball1 = display.newImage("Images/super_ball.png", 0, 0)
 
 	-- add to physics
 	physics.addBody(ball1, {density=1.0, friction=0.5, bounce=0.3, radius=25})
 end
 -----------------------------------------------------------------
 local function SecondBall()
-	local ball2 = display.newImage("physics/super_ball.png", 0, 0)
+	local ball2 = display.newImage("Images/super_ball.png", 1000, 0)
 
 	--add to physics
-	physics.addBody(ball2, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
+	physics.addBody(ball2, {density=3, friction=0.5, bounce=0.3, radius=20})	
 end
 
 ---------------------------------------------------------------------
 --TIMER DELAYS - call each function after the given millisecond
 ----------------------------------------------------------------------
 timer.performWithDelay(0, firstBall)
+
 timer.performWithDelay(500, SecondBall)
 
